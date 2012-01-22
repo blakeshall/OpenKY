6cd2e43dc2c7b11dbb8fdc7bde8c62e65ffcaecb
Drew Fitzpatrick <socialengineerdude@gmail.com>
2012-01-22 00:15:15 -0500

    Refactored OpenStates class and deleted old code

--- app.rb
+++ app.rb
@@ -4,76 +4,8 @@ require 'json'
 require 'httparty'
 require 'uri'
 require 'pp'
+load "models/open_states_wrapper.rb" 
 
-API_KEY = '15976b43616b43298ea1a3a98971d3e0'
-class OpenStates
-  include HTTParty
-  base_uri 'http://openstates.org/api/v1/'
-  
-  def legislator_search(options={})
-    url = "/legislators/?state=KY"
-    url += "&first_name=#{options[:first_name]}" 
-    url += "&last_name=#{options[:last_name]}" 
-    url += "&chamber=#{options[:chamber]}" 
-    url += "&active=#{options[:active]}" 
-    url += "&term=#{options[:term]}" 
-    url += "&district=#{options[:district]}" 
-    url += "&party=#{options[:party]}" 
-    
-    url += "&apikey=" + API_KEY
-    puts("!!!!!!!!!!!!!")
-    puts(url)
-    self.class.get(url)
-  end
-  
-  def legislator_lookup(options={})
-    url = "/legislators/"
-    url += "#{options[:leg_id]}/"
-    url += "?apikey=#{API_KEY}"
-    self.class.get(url)
-  end
-
-  def bill_search(options ={})
-    url = "/bills/?state=KY&q=#{options[:keyword]}"
-    url += "&chamber=#{options[:chamber]}"
-    if !options[:subject].nil?
-      url += "&subject=#{URI.encode(options[:subject])}"
-    end
-    url += "&apikey=" + API_KEY
-    self.class.get(url)
-  end
-  
-  def bill_lookup(options={})
-    url = "/bills/ky/"
-    url += "#{options[:session]}/"
-    url += "#{options[:bill_id]}/"
-    url += "?apikey=#{API_KEY}"
-    self.class.get(URI.encode(url))
-  end
-  
-  # Committee search not needed
-  #def committee_search(options={})
-  #  url = "/committees/?state=ky"
-  #  if !options[:committee].nil?
-  #    url += "&committee=#{URI.encode(options[:committee])}"
-  #  end
-  #  if !options[:subcommittee].nil?
-  #    url += "&subcommittee=#{URI.encode(options[:subcommittee])}"
-  #  end
-  #  if !options[:chamber].nil?
-  #   url += "&chamber=#{options[:chamber]}"
-  #  end
-  #  url += "&apikey=" + API_KEY
-  #  self.class.get(url)
-  #end
-  
-  def committee_lookup(com_id)
-    url = "/committees/#{com_id}/?apikey=#{API_KEY}"
-    self.class.get(url)
-  end
-
-
-end
 
 
 get '/' do
@@ -90,7 +22,6 @@ get '/legislator/search/' do
   erb :legislator_results
 end
 
-# TODO: Implement legislator_detail.erb
 get '/legislators/:leg_id' do
   @legislator = OpenStates.new.legislator_lookup({:leg_id => params['leg_id']});
   erb :legislator_detail
@@ -112,19 +43,6 @@ get '/bills/:session/:bill_id' do
   @bill = OpenStates.new.bill_lookup({:session => params[:session], :bill_id => params[:bill_id]})
   erb :bill_detail
 end
-
-# Removed committee search stuff as it is not really needed
-#committee search form
-#get '/committee_search' do
-#  erb :committee_search
-#end
-
-#Committee Search Results
-#get '/committee/search/' do
-#  @results = OpenStates.new.committee_search({:committee => params[:committee], :subcommittee => params[:subcommittee], :chamber => params[:chamber]})
-#  pp(@results)
-#  erb :committee_results
-#end
 
 get '/committee/:com_id' do
   @committee = OpenStates.new.committee_lookup(params[:com_id])
---
+++ models/open_states_wrapper.rb
@@ -0,0 +1,55 @@
+
+API_KEY = '15976b43616b43298ea1a3a98971d3e0'
+
+class OpenStates
+  include HTTParty
+  base_uri 'http://openstates.org/api/v1/'
+  
+  def legislator_search(options={})
+    url = "/legislators/?state=KY"
+    url += "&first_name=#{options[:first_name]}" 
+    url += "&last_name=#{options[:last_name]}" 
+    url += "&chamber=#{options[:chamber]}" 
+    url += "&active=#{options[:active]}" 
+    url += "&term=#{options[:term]}" 
+    url += "&district=#{options[:district]}" 
+    url += "&party=#{options[:party]}" 
+    
+    url += "&apikey=" + API_KEY
+    puts("!!!!!!!!!!!!!")
+    puts(url)
+    self.class.get(url)
+  end
+  
+  def legislator_lookup(options={})
+    url = "/legislators/"
+    url += "#{options[:leg_id]}/"
+    url += "?apikey=#{API_KEY}"
+    self.class.get(url)
+  end
+
+  def bill_search(options ={})
+    url = "/bills/?state=KY&q=#{options[:keyword]}"
+    url += "&chamber=#{options[:chamber]}"
+    if !options[:subject].nil?
+      url += "&subject=#{URI.encode(options[:subject])}"
+    end
+    url += "&apikey=" + API_KEY
+    self.class.get(url)
+  end
+  
+  def bill_lookup(options={})
+    url = "/bills/ky/"
+    url += "#{options[:session]}/"
+    url += "#{options[:bill_id]}/"
+    url += "?apikey=#{API_KEY}"
+    self.class.get(URI.encode(url))
+  end
+ 
+  def committee_lookup(com_id)
+    url = "/committees/#{com_id}/?apikey=#{API_KEY}"
+    self.class.get(url)
+  end
+
+end
+
